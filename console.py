e2139669cebaf40938242eedfce6bf13cf697ef6
Student <student@Students-MacBook-Pro.local>
2022-06-13 19:15:17 +0100

    Added more books and publishers in console.py

--- console.py
+++ console.py
@@ -14,6 +14,12 @@ publisher_repository.save(publisher1)
 publisher2 = Publisher("Penguin Books Ltd", "One Embassy Gardens, 8 Viaduct Gardens, London, SW11 7BW.", "020 7139 3000")
 publisher_repository.save(publisher2)
 
+publisher3 = Publisher("Hachette UK", "Carmelite House, 50 Victoria Embankment, London, EC4Y 0DZ", "020 3122 6000")
+publisher_repository.save(publisher3)
+
+publisher4 = Publisher("Transworld Publishers Ltd", "20 Vauxhaull Bridge Road, London, SW1V 2SA", "020 7840 8400")
+publisher_repository.save(publisher4)
+
 book1 = Book("The Lord of the Rings", "JRR Tolkien", "fantasy", 9, 8.50, 16.99, publisher1, "9780261102385", "paperback")
 book_repository.save(book1)
 
@@ -22,5 +28,14 @@ book_repository.save(book2)
 
 book3 = Book("The Silmarillion", "JRR Tolkien", "fantasy", 0, 8.50, 16.99, publisher1, "9780008433956", "hardback")
 book_repository.save(book3)
+
+book4 = Book("The Girl with the Dragon Tattoo", "Steig Larsson", "crime", 12, 4.44, 8.99, publisher3, "9780857054036", "paperback")
+book_repository.save(book4)
+
+book5 = Book("Dracula", "Bram Stoker", "horror", 2, 7.50, 14.99, publisher2, "9780141196886", "hardback")
+book_repository.save(book5)
+
+book6 = Book("Thud!", "Terry Pratchett", "fantasy", 3, 4.40, 8.99, publisher4, "9780552167697", "paperback")
+book_repository.save(book6)
 
 pdb.set_trace()
